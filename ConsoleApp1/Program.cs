ba05e578c317ef77a8066c8a1de04ca59555cf34
Natrium14 <natrium744.2@gmail.com>
2022-12-28 14:35:06 +0700

    тест изменений

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using ConsoleApp1.Model.Entities;
+using System.Text.Json;
 
 namespace ConsoleApp1
 {
@@ -10,6 +12,139 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
+            /*
+            using(var db = new Entities())
+            {
+                var employees = db.Сотрудники.ToList();
+                foreach(var emp in employees)
+                {
+                    Console.WriteLine(emp.Имя);
+                }
+            }
+            */
+
+            Citizen citizen = new Citizen("Alex", "Times", new DateTime(1991, 1, 1), "6576-989898");
+            citizen.Address = "Tomsk, Lenina 30";
+
+            Console.WriteLine(citizen.GetInfo());
+            
+            Person a = new Person("Bob");
+            string json = JsonSerializer.Serialize(a);
+            Console.WriteLine(json);
+            Person restoredCitizen = JsonSerializer.Deserialize<Person>(json);
+            Console.WriteLine(restoredCitizen.GetInfo());
+
+            Console.Read();
+        }
+    }
+
+    class Person
+    {
+        protected string _name;
+        protected string _surname;
+        protected DateTime _dateBirth;
+
+        public Person()
+        {
+            _name = "-";
+            _surname = "-";
+        }
+        public Person(string name)
+        {
+            _name = name;
+            _surname = "-";
+        }
+        public Person(string name, string surname)
+        {
+            _name = name;
+            _surname = surname;
+        }
+        public Person(string name, string surname, DateTime dateBirth)
+        {
+            _name = name;
+            _surname = surname;
+            _dateBirth = dateBirth;
+        }
+
+        public string Name
+        {
+            get
+            {
+                return _name;
+            }
+            set
+            {
+                _name = value;
+            }
+        }
+        public string Surname
+        {
+            get
+            {
+                return _surname;
+            }
+            set
+            {
+                _surname = value;
+            }
+        }
+        public int CurrentAge
+        {
+            get
+            {
+                return DateTime.Now.Year - _dateBirth.Year;
+            }
+        }
+
+        public string GetFullName()
+        {
+            return _name + " " + _surname;
+        }
+        public virtual string GetInfo()
+        {
+            return _name + " " + _surname + " " + CurrentAge;
+        }
+    }
+
+    class Citizen: Person
+    {
+        protected string _passport;
+        protected string _address;
+
+        public Citizen(string name, string surname, DateTime dateBirth, string passport)
+            :base(name, surname, dateBirth)
+        {
+            _passport = passport;
+        }
+        public Citizen(string name, string surname, DateTime dateBirth, string passport, string address)
+           : base(name, surname, dateBirth)
+        {
+            _passport = passport;
+            Address = address;
+        }
+
+        public string Passport { get => _passport; }
+        public string Address { get => _address; set => _address = value; }
+
+        public override string GetInfo()
+        {
+            return " Name: " + _name 
+                + "\n Surname: " + _surname 
+                + "\n Age: " + CurrentAge 
+                + "\n Passport: " + Passport
+                + "\n Address: " + Address
+                + "\n-----";
+        }
+    }
+
+    class Client: Citizen
+    {
+        protected double _balance;
+
+        public Client(string name, string surname, DateTime dateBirth, string passport, string address, double balance)
+           : base(name, surname, dateBirth, passport, address)
+        {
+            _balance = balance;
         }
     }
 }
